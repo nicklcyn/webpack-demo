f3babac4882eb7067d8c71e6dd0249295e4fac73
nicklcyn <45242556+nicklcyn@users.noreply.github.com>
2020-10-23 17:56:52 +0800

    Create login.js

---
+++ src/login.js
@@ -0,0 +1,27 @@
+import { success, faile } from './alert'
+
+
+var login = function() {
+    
+    const username = document.querySelector("#username-field").value;
+    const password = document.querySelector("#password-field").value;
+
+    return username === 'user' && password === '##OOXX'
+}
+
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(login()) {
+        success();
+        location.reload();
+    } else {
+        faile();
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+
+
